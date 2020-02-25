cf454a52b6e7dc6b65b3f6ed23e4661d6021261f
ISEIUPA <luis.ernesto.anaya@upa.edu.mx>
2020-02-24 22:12:02 -0600

    Generador de modal, solo titulo

--- js/modals.js
+++ js/modals.js
@@ -1,27 +1,31 @@
 
 function modalClick(idClicked)
 {
-
     // Get the modal
-    var modal = document.getElementById("myModal");
-    var modalContent = document.getElementsByClassName("modal-content")[0];
+    var modal = document.getElementById("myVentana");
+    var modalContent = document.getElementsByClassName("ventana-contenido")[0];
 
     var btn = document.getElementById(idClicked);
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
+    var span = document.getElementsByClassName("closeBtn")[0];
+ 
     btn.onclick = function() {
-        materiaArray = modalText(idClicked);
-        console.log(materiaArray);
-        modalContent.innerHTML = "<h3>" + materiaArray.materia + "</h3>";
+//        readTextFile(function(text){
+//            var data = JSON.parse(text);
+//            window.actual_JSON = data[idClicked][0];
+//            console.log(window.actual_JSON);
+//       });
+        //var datosModal = window.actual_JSON;
+        
+        datosModal = {"materias" : "Ingles J", "competencias" : ["Competencia 1", "Competencia 2"]};
+        modalContent.innerHTML = "";
+        modalContent.innerHTML += "<h3>" + datosModal.materias + "</h3>";
+        modalContent.innerHTML += "<p>" + datosModal.competencias + "</p>";
         modal.style.display = "block";
     }
 
-    // When the user clicks on <span> (x), close the modal
+    //When the user clicks on <span> (x), close the modal
     span.onclick = function() {
-      modal.style.display = "none";
+        modal.style.display = "none";
     }
 
     // When the user clicks anywhere outside of the modal, close it
@@ -32,14 +36,16 @@ function modalClick(idClicked)
     }
 }    
 
-function modalText(idClicked){
-    //Crear JSON con los datos y direccionarlos por el idClicked
-    fetch("js/materias.json")
-            .then(function(resp){
-                return resp.json();
-            })
-            .then(function(data){
-                console.log(data[idClicked][0]);
-            return data;
-            });
+function readTextFile(callback){
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", 'js/materias.json', true);
+    rawFile.onreadystatechange = function(){
+        if (rawFile.readyState === 4 && rawFile.status == "200"){
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
 }
+
+
