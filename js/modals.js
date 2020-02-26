406815f4af27fddabba6bf52d18672f69bbd0079
ISEIUPA <luis.ernesto.anaya@upa.edu.mx>
2020-02-25 18:35:41 -0600

    Todas las materias con link, cabecera en plan de estudios

--- js/modals.js
+++ js/modals.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+    readTextFile(function(text){
+            var data = JSON.parse(text);
+            window.actual_JSON = data;
+       });
+});
+
 
 function modalClick(idClicked)
 {
@@ -7,38 +14,29 @@ function modalClick(idClicked)
 
     var btn = document.getElementById(idClicked);
     var span = document.getElementsByClassName("closeBtn")[0];
- 
-    btn.onclick = function() {
-//        readTextFile(function(text){
-//            var data = JSON.parse(text);
-//            window.actual_JSON = data[idClicked][0];
-//            console.log(window.actual_JSON);
-//       });
-        //var datosModal = window.actual_JSON;
-        
-        datosModal =
-        {
+    //var datosModal = window.actual_JSON[idClicked][0];
+    
+    
+    datosModal =   {
             "Materia": "Inglés I",
-            "cuatrimestre": "Primer Cuatrimestre",
+            "Cuatrimestre": "Primer Cuatrimestre",
             "NumUnidades":4,
             "Unidades": ["Introducción: Interacción en el aula" , "Descripción y ubicación de lugares y objetos", "Habilidades y acciones cotidianas", "Comunicar opiniones" ],
-            "Objetivo": "El alumno será capaz de manejar, comunicar y comprender ideas y/o textos simples abordando eventos cotidianos con bajo nivel de complejidad haciendo uso de la información de su entorno" 
+            "Objetivo": "El alumno será capaz de manejar, comunicar y comprender ideas y/o textos simples abordando eventos cotidianos con bajo nivel de complejidad haciendo uso de la información de su entorno"    
         };
-        
-        
-        modalContent.innerHTML = "";
-        modalContent.innerHTML += '<h3 class="modalH3">' + datosModal.Materia + '</h3>';
-        modalContent.innerHTML += '<h4 class="modalH4">Objetivo: ' + datosModal.Objetivo + '</h4>';
-        modalContent.innerHTML += '<h4 class="modalH4">Unidades de aprendizaje: </h4>';
-        var lista = '<ol class="modalList">';
-        for (var i =0; i< datosModal.Unidades.length; i++){
-            lista += '<li>' + datosModal.Unidades[i] + '</li>';}
-        lista += "</ol>";
-        console.log(lista);
-        modalContent.innerHTML += lista;
-        modal.style.display = "block";
-    }
-
+    
+    
+    modalContent.innerHTML = "";
+    modalContent.innerHTML = '<h3 class="modalH3">' + datosModal.Materia + '</h3>';
+    modalContent.innerHTML += '<h4 class="modalH4">Objetivo: ' + datosModal.Objetivo + '</h4>';
+    modalContent.innerHTML += '<h4 class="modalH4">Unidades de aprendizaje: </h4>';
+    var lista = '<ol class="modalList">';
+    for (var i =0; i< datosModal.Unidades.length; i++){
+        lista += '<li>' + datosModal.Unidades[i] + '</li>';}
+    lista += "</ol>";
+    modalContent.innerHTML += lista;
+    modal.style.display = "block";
+    
     //When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
